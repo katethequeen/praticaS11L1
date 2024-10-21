5e6926a1655b3e9d1f07d7da6e17f13f060ad490
katethequeen <cateresa4@gmail.com>
2024-10-21 16:42:43 +0200

    Add First Part

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./components/page1/page1.module').then((m) => m.Page1Module),
+  },
+  {
+    path: 'page2',
+    loadChildren: () =>
+      import('./components/page2/page2.module').then((m) => m.Page2Module),
+  },
+  {
+    path: 'page3',
+    loadChildren: () =>
+      import('./components/page3/page3.module').then((m) => m.Page3Module),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
